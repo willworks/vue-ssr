ea40f7ef5cadb730c9f2f07227859fec898df329
willworks <scnuzhonghanjin@gmail.com>
2016-11-12 14:39:54 +0800

    feat: add coverage&mocha for the middlewares, specific test units is to be done

--- router.js
+++ router.js
@@ -1,19 +1,19 @@
 import Router from 'koa-router';
 import demo from './controllers/demo';
+import test from './controllers/test';
 
 const router = new Router();
 
 // ====== demo ======
-
-// ====== spa entence ======
 router.get('/', demo.index);
-
-// ====== api ======
 router.get('/api/demo', demo.demo);
 router.get('/api/setDemo', demo.setDemo);
 router.get('/api/getDemo', demo.getDemo);
 router.get('/api/delDemo', demo.delDemo);
 router.get('/api/updateDemo', demo.updateDemo);
+
+// ====== test units ======
+router.get('/test', test.index);
 
 // ====== app route ======
 
---
+++ test/middlewares/finalHandler.test.js
@@ -0,0 +1,14 @@
+// TODO
+const assert = require('power-assert');
+const request = require('supertest');
+
+describe('test/middlewares/finalHandler.test.js', () => {
+  
+  it('it is a case true for test unit', function* () {
+    // const result = yield request()
+    //   .get('/test')
+    //   .set('Accept', 'application/json')
+    //   .expect(200);
+    assert(true);
+  });
+});
