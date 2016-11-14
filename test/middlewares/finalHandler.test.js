40a6c55dd325c8b517a5b354385b633cfd4fd2d8
willworks <scnuzhonghanjin@gmail.com>
2016-11-14 14:42:00 +0800

    feat: use koa instance to test middlewares(still to be done)

--- test/middlewares/finalHandler.test.js
+++ test/middlewares/finalHandler.test.js
@@ -1,14 +1,63 @@
-// TODO
-const assert = require('power-assert');
-const request = require('supertest');
+'use strict';
+
+var http = require('http');
+var Router = require('koa-router');
+var koa = require('koa');
+var app = new koa();
+var supertest = require('supertest');
+
+var instance = function () {
+  var router = Router();
+  router.get('/test', function * (next) {
+    this.body = 200;
+    yield next;
+  });
+  router.post('/test', function * (next) {
+    console.log(this.request.body);
+    this.body = {status: 200};
+    yield next;
+  });
+  const server = http.createServer(app.callback());
+  return server;
+};
 
 describe('test/middlewares/finalHandler.test.js', () => {
   
-  it('it is a case true for test unit', function* () {
-    // const result = yield request()
-    //   .get('/test')
-    //   .set('Accept', 'application/json')
-    //   .expect(200);
-    assert(true);
+  var agent, server;
+  var data = {test: 123};
+  
+  beforeEach(function () {
+    server = instance();
+    agent = supertest(server.listen());
   });
+  
+  afterEach(function (done) {
+    server.close(done);
+  });
+  
+  describe('get', () => {
+    it('returns ok', (done) => {
+      agent.get('/test').expect(200, done);
+    });
+    it('returns ok', (done) => {
+      agent.get('/test').expect(200, done);
+    });
+  });
+  
+  describe('post 1', () => {
+    it('returns ok', (done) => {
+      agent.post('/test')
+        .send(data)
+        .expect(200, done);
+    });
+  });
+  
+  describe('post 2', () => {
+    it('returns ok', (done) => {
+      agent.post('/test')
+        .send(data)
+        .expect(200, done);
+    });
+  });
+  
 });
